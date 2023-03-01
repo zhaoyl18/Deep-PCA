7db4d2a8e315a71955df0f9d4a576957f4c8749d
Yulai Zhao <zhaoyl18thu@gmail.com>
2023-03-01 00:25:24 -0500

    update baby face

--- LSE-matrix.py
+++ LSE-matrix.py
@@ -6,34 +6,36 @@ import torch.nn.functional as F
 from torchvision import utils, datasets, transforms
 import matplotlib.pyplot as plt
 
+from PIL import Image
+ 
+ 
+# load the image and convert into
+# numpy array
+
 
 def main(args):
 	kwargs = {}
-	train_loader = torch.utils.data.DataLoader(
-		datasets.CIFAR10('./data.cifar10', train=True, download=False,
-					   transform=transforms.Compose([
-						   transforms.ToTensor(),
-						   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-					   ])),
-		batch_size=1, shuffle=False, **kwargs)
+	img = Image.open('baby_mini_d3_gaussian.jpg')
+	img_data = torch.from_numpy(np.asarray(img)).double()
+	# view_image(img_data)
+	# img = utils.make_grid(img_data)
+	# plt.imshow(img_data) 
+	# plt.show()
 
-	dataiter = iter(train_loader)
-	data, lbls = next(dataiter)
-	
-	target = F.one_hot(lbls, 10)
+	data = img_data.unsqueeze(0)/255
+	data = data*2 - 1 # normalize to [-1, 1]
+	data = data.permute(0, 3, 1, 2)
 
 	# data: shape [batch, 3, H, W] where 3 means RGB channels, batch=10, H=W=32
 
 	view_image(data[0]) # visualize the first image in the batch (optional)
 
-	raw_img = torch.from_numpy(data.numpy()) # save a copy of raw image shape
+	raw_img = data.clone() # save a copy of raw image shape
 	compressed_img = torch.nn.functional.interpolate(data, scale_factor=0.5, mode='bilinear')
 	X_data = torch.nn.functional.interpolate(compressed_img, scale_factor=2, mode='nearest')	
 	
-	if args.model == 'LSE':
-		LSE((X_data, raw_img))
-	elif args.model == 'CONV':
-		naive_conv(tuple(X_data, raw_img))
+	LSE((X_data, raw_img))
+
 
 
 def LSE(inputs):
@@ -44,7 +46,7 @@ def LSE(inputs):
 	kernel_weights = torch.matmul(torch.inverse(torch.matmul(stem.t(), stem)+1e-3*torch.eye(stem.shape[-1])),
 			        torch.matmul(stem.t(), targets))
 	conved = torch.matmul(stem, kernel_weights).reshape(B, H, W, C).permute(0,3,1,2)
-	view_image(conved[0])
+	view_image(torch.clamp(conved[0], min=-1.0, max=1.0))
 	# kernel_weights = kernel_weights.reshape(3, 3, 3, 3) # (in_channels, kernel_size, kernel_size, out_channels,)
 	# kernel_weights = kernel_weights.permute(0, 3, 1, 2) # (in_channels, out_channels, kernel_size, kernel_size)
 	return kernel_weights
@@ -92,23 +94,6 @@ def naive_conv(inputs): # not completed yet..
 	prediction = model(X_data)
 	view_image(prediction[0]) # visualize the first image in the batch (optional)
 
-	# print('\nAccuracy: {:2.2f} %'.format(accuracy*100))
-
-# class compression(torch.nn.Module):
-
-#     def __init__(self):
-#         super(compression, self).__init__()
-#         self.conv = torch.nn.Sequential(
-#             torch.nn.Conv2d(3, 3, kernel_size=3, stride=1, padding=0),
-#             torch.nn.Dropout(p=0.5))
-
-#     def forward(self, x):
-#         out = self.conv(x)
-#         out = out.view(out.size(0), -1)   # Flatten them for FC
-#         return out
-
-
-# TODO: write your STEM function
 def STEM(inputs, kernel=3):
 	'''
 	hint: stem should have shape [batchxHxW, 3xkernelxkernel] where batch=10, H=W=32, kernel=3
@@ -122,9 +107,9 @@ def STEM(inputs, kernel=3):
 				outputs[b*H*W + i*W + j] = X_pad[b, :, i:i+kernel, j:j+kernel].reshape(-1)
 	return outputs
 
-def view_image(tensor):
-	image = utils.make_grid(tensor)
-	img = image / 2 + 0.5   # unnormalize
+def view_image(tensor):  # input: (C, H, W)
+	img = utils.make_grid(tensor)
+	img = img / 2 + 0.5   # unnormalize
 	npimg = img.numpy()   # convert from tensor
 	plt.imshow(np.transpose(npimg, (1, 2, 0))) 
 	plt.show()
