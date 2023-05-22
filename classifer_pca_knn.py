3ca45aef36f93cc6f537c289bfddf59b54cd8ee5
AllisonShen <xiaxin.shen@outlook.com>
2023-05-22 00:54:33 -0400

    Done

--- classifer_pca_knn.py
+++
@@ -1,6 +0,0 @@
-from sklearn import decomposition, datasets
-from sklearn import neighbors
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import GridSearchCV, cross_val_score
-from sklearn.preprocessing import StandardScaler
-
